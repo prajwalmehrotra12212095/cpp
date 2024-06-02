20c320845499d6ab0c93668cd739018fecd98f93
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2024-06-03 01:35:04 +0530

    hg

---
+++ main/for.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+// for(int i=0; i<=5;i=i+1){
+//     cout<<"hey"<<endl;;
+// }
+for(int i = 5;i>0;i=i-1){
+    cout<<i<<endl;
+}
+    return 0;
+}

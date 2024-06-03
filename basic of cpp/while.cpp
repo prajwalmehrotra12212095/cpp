ffd93744a8b8442af7c5ffaba75bb7d8753b02ca
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2024-06-04 03:09:33 +0530

    messas

---
+++ basic of cpp/while.cpp
@@ -0,0 +1,30 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+// int i=5;
+// // while(i>=0){
+// //     cout<<"hey"<<i<<endl;
+// //     i=i-1;
+// // }
+// //    return 0; 
+// // }
+// do{
+//     cout<<"hey"<<i<<endl;
+//     i = i+1;
+
+// }while(i<=1);
+// cout<<"g"<<endl;
+// return 0;
+// }
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 10;
+    for (int i = 1; i <= 10; i++) {
+        cout << n * i << " ";
+    }
+    cout << endl;
+    return 0;
+}
+

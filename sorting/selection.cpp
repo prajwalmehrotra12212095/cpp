28fe463797ff075797298656e3a3a36f9190d9fd
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2025-01-07 02:18:20 +0530

    mdff

--- sorting/selection.cpp
+++ sorting/selection.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 void selection_sort(int arr[],int n){
-for(int i=0;i<n-2;i++){
+for(int i=0;i<n-1;i++){
 int mini= i;
-for(int j=i;j<=n-1;j++){
+for(int j=i+1;j<=n;j++){
 if(arr[j]<arr[mini]){
     mini = j; 
 }
@@ -15,7 +15,7 @@ arr[i] = temp;
 }
 int main(){
     int n;
-    cin>>n;
+    cin>>n; 
     int arr[n];
     for(int i=0;i<n;i++)cin>>arr[i];
     selection_sort(arr,n);

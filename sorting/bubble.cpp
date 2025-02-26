96cfa424ab4323215c1dd9ba5f63dd8b65df21b7
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2025-02-26 14:16:06 +0530

    mesage

--- sorting/bubble.cpp
+++ sorting/bubble.cpp
@@ -15,7 +15,9 @@ int main(){
     int n;  
     cin>>n; 
     int arr[n];
-    for(int i=0;i<n;i++)cin>>arr[i];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
     bubble_sort(arr,n);
     for(int i =0;i<n;i++){
         cout<<arr[i]<<" ";

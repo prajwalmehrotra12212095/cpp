d810698814402ad19d2cf8fbed26717363494ce7
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2025-03-04 03:07:41 +0530

    message

---
+++ binarysearch/binary.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr[],int n,int key){
+    int start =0;
+    int end = n-1;
+    int mid  = (start+end)/2;
+    while(start<=end){
+
+        if(arr[mid] == key){
+            return mid;
+        }
+        if(key > arr[mid]){
+           start =  mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+        mid = (start+end)/2;
+    }
+    return -1;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i =0;i<n;i++){
+        cin>>arr[i];
+    }
+    int key;
+    cout<<"enter the key"<<endl;
+    cin>>key;
+    // int index = 
+    cout<<binarySearch(arr,n,key);
+   // cout<<"index is"<<index<<endl;
+   
+    
+}

fbd64412a9f5abbdd644ccbc4e333a7f1243fe40
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2025-04-05 02:09:04 +0530

    mesafe

--- strings/reverserstring.cpp
+++ strings/reverserstring.cpp
@@ -1,2 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+    
+    int start = 0;
+    int end = n - 1;
+
+    while (start < end) {
+        swap(str[start], str[end]);
+        start++;
+        end--;
+    }
+
+    cout << str;
+    return 0;
+}

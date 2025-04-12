53af3b3bac5a39ef0893a99d4fbcbc249fc7033c
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2025-04-13 02:54:58 +0530

    mesafe

---
+++ linked list/part1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+ class Node{
+    public:
+    int data;
+    Node* next;
+
+//constructor
+
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+ };
+
+ class List{
+    Node* head;
+    Node* tail;
+
+    public:
+    List(){
+        head = tail = NULL;
+    }
+ };
+
+ int main(){
+    Node* node1 = new Node();
+    cout<<node1 -> data<<endl;
+    cout<<node1 -> next<<endl;
+    
+    return 0;
+ }
